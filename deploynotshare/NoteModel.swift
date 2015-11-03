3f59077f8b99a6e51cd18c7b72f0d8c9d69e8cd0
Chintan <chintan@wohlig.com>
2015-11-03 16:01:16 +0530

    changes in notemodel

--- deploynotshare/NoteModel.swift
+++ deploynotshare/NoteModel.swift
@@ -13,7 +13,7 @@ public class Note {
     
     
     public let db = AppDelegate.getDatabase()
-    public let Note = Table("note")
+    public let note = Table("note")
     public let id = Expression<Int64>("id")
     public let title = Expression<String?>("title")
     public let creationTime = Expression<Int64>("creationTime")
@@ -24,14 +24,14 @@ public class Note {
     public let folder = Expression<Int64>("folder")
     public let islocked = Expression<Int64>("islocked")
     public let paper = Expression<String?>("paper")
-    public let remindertime = Expression<Int64>("remindertime")
+    public let reminderTime = Expression<Int64>("reminderTime")
     public let serverid = Expression<String?>("serverid")
     public let tags = Expression<String?>("tags")
-    public let timebomb = Expression<String?>("timebomb")
+    public let timebomb = Expression<Int64>("timebomb")
     
     init() {
         
-        try! db.run(Note.create(ifNotExists: true) { t in
+        try! db.run(note.create(ifNotExists: true) { t in
             t.column(id, primaryKey: .Autoincrement)
             t.column(title)
             t.column(creationTime)
@@ -41,7 +41,7 @@ public class Note {
             t.column(folder)
             t.column(islocked)
             t.column(paper)
-            t.column(remindertime)
+            t.column(reminderTime)
             t.column(serverid)
             t.column(tags)
             t.column(timebomb)
@@ -49,28 +49,36 @@ public class Note {
         
     }
     
-    func create(name2:String) -> AnySequence<Row>  {
-        let insert = folder.insert( name <- name2, creationTime <- 12345, modificationTime <- 12345, order <- 1, serverID <- "123")
+    func create(title2:String,background2:String,color2:String,folder2:Int64,islocked2:Int64,paper2:String,reminderTime2:Int64,serverid2:String,tags2:String,timebomb2:Int64) {
+        let date = NSDate().timeIntervalSince1970
+        let insert = note.insert( title <- title2, creationTime <- Int64(date), modificationTime <- Int64(date), background <- background2, color <- color2, folder <- folder2, islocked <- islocked2,paper <- paper2 , reminderTime <- reminderTime2, serverid <- serverid2, tags <- tags2 , timebomb <- timebomb2)
         try! db.run(insert)
-        return try! db.prepare(folder)
     }
     
     func find() -> AnySequence<Row>  {
-        return try! db.prepare(folder)
+        let date = NSDate().timeIntervalSince1970
+        let date2 = Int64(date)
+        return try! db.prepare(note.filter(creationTime != 0 && (timebomb > date2 ||  timebomb == 0)  ))
     }
     
-    func edit(name2:String,id2:String) -> AnySequence<Row>  {
+    func edit(title2:String,background2:String,color2:String,folder2:Int64,islocked2:Int64,paper2:String,reminderTime2:Int64,serverid2:String,tags2:String,timebomb2:Int64,id2:String)  {
+        let date = NSDate().timeIntervalSince1970
         let id3 = strtoll(id2,nil,10)
-        let fol = folder.filter(id == id3)
-        try! db.run(fol.update(name <- name2))
-        return try! db.prepare(folder)
+        let note2 = note.filter(id == id3)
+        try! db.run(note2.update(title <- title2, creationTime <- Int64(date), modificationTime <- Int64(date), background <- background2, color <- color2, folder <- folder2, islocked <- islocked2,paper <- paper2 , reminderTime <- reminderTime2, serverid <- serverid2, tags <- tags2 , timebomb <- timebomb2))
     }
     
-    func delete(id2:String) -> AnySequence<Row>  {
+    func delete(id2:String) {
+        let date = NSDate().timeIntervalSince1970
         let id3 = strtoll(id2,nil,10)
-        let fol = folder.filter(id == id3)
-        try! db.run(fol.delete())
-        return try! db.prepare(folder)
+        let note2 = note.filter(id == id3)
+        try! db.run(note2.update(title <- "", creationTime <- 0, modificationTime <- Int64(date), background <- "", color <- "", folder <- 0, islocked <- 0,paper <- "" , reminderTime <- 0, serverid <- "", tags <- "" , timebomb <- 0))
+    }
+    
+    func getNotesFolder(folder2:Int64) -> AnySequence<Row>  {
+        let date = NSDate().timeIntervalSince1970
+        let date2 = Int64(date)
+        return try! db.prepare(note.filter(creationTime != 0 && (timebomb > date2 ||  timebomb == 0)  &&  folder == folder2 ))
     }
     
     
