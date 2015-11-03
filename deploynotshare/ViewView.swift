0ba6ee216909408d1aa49743ad643fdba5b9ebe6
jagruti181 <patiljagruti181@gmail.com>
2015-11-03 11:27:15 +0530

    git

---
+++ deploynotshare/ViewView.swift
@@ -0,0 +1,21 @@
+//
+//  ViewView.swift
+//  deploynotshare
+//
+//  Created by Jagruti Patil on 03/11/15.
+//  Copyright © 2015 Wohlig. All rights reserved.
+//
+
+import UIKit
+
+class ViewView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
