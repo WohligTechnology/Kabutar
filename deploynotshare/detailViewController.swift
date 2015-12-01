a38e70312b515057768d139f935953154c5269b2
Chintan <chintan@wohlig.com>
2015-12-01 13:42:53 +0530

    image resize

--- deploynotshare/detailViewController.swift
+++ deploynotshare/detailViewController.swift
@@ -363,8 +363,9 @@ class detailViewController: UIViewController , UINavigationControllerDelegate,UI
         self.presentViewController(alert, animated: true, completion: nil)
     }
     
-    func appendImage(image: UIImage!, isnew:Bool) {
-        if(image != nil) {
+    func appendImage(image2: UIImage!, isnew:Bool) {
+        let image  = image2.resizeToWidth(500)
+        if(image2 != nil) {
             let newheight = width / image.size.width * image.size.height
             let imageView = ElementImage(frame: CGRectMake(0,0,width+10,newheight))
             imageView.contentMode = .ScaleAspectFit
@@ -597,6 +598,31 @@ extension String {
     func stripHTML() -> String {
         let abc = self.stringByReplacingOccurrencesOfString(htmlReplaceString, withString: " ", options: NSStringCompareOptions.RegularExpressionSearch, range: nil)
         return abc.stringByReplacingOccurrencesOfString("&nbsp;", withString:" ")
+    }
+}
+
+
+
+extension UIImage {
+    func resize(scale:CGFloat)-> UIImage {
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: size.width*scale, height: size.height*scale)))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        imageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
+    }
+    func resizeToWidth(width:CGFloat)-> UIImage {
+        let imageView = UIImageView(frame: CGRect(origin: CGPoint(x: 0, y: 0), size: CGSize(width: width, height: CGFloat(ceil(width/size.width * size.height)))))
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        imageView.image = self
+        UIGraphicsBeginImageContext(imageView.bounds.size)
+        imageView.layer.renderInContext(UIGraphicsGetCurrentContext()!)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
     }
 }
 
