eed4022f256ed6119e925d233515a368f9f51100
Harsh Thakkar <harshthakkar46@gmail.com>
2016-02-06 14:08:34 +0530

    views added and icons changed
    notification view, share noteshare view, share view

--- deploynotshare/ShareView.swift
+++ deploynotshare/ShareView.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ShareView: UIView {
     
+    @IBOutlet var shareNoteshare: UIButton!
+    @IBOutlet var shareText: UIButton!
+    @IBOutlet var shareSreenshot: UIButton!
+    @IBOutlet var shareUrl: UIButton!
+    
     @IBOutlet var shareview: UIView!
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         NSBundle.mainBundle().loadNibNamed("ShareView", owner: self, options: nil)
         self.addSubview(self.shareview)
     }
+    
+    func shareViaNoteshare() {}
+    
+    func shareViaText() {}
+    
+    func shareViaSreenshot () {}
+    
+    func shareViaUrl () {}
     
 }
