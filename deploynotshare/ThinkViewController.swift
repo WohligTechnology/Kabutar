e5b8a254ab03def0333f85f61a93ed37f1cd8c2d
Harsh Thakkar <harshthakkar46@gmail.com>
2016-04-12 15:34:53 +0530

    scroll added to feedback page

--- deploynotshare/ThinkViewController.swift
+++ deploynotshare/ThinkViewController.swift
@@ -13,46 +13,70 @@ import SwiftyJSON
 class ThinkViewController: UIViewController {
     
 //    var mainViewController: UIViewController!
+    var keyboardHeight: CGFloat!
+    var keyboardOn = false
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var tellusLabel: UILabel!
     @IBOutlet weak var saysomething: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNavigationBarItem()
 //        self.setNavigationBarItem()
+        self.tellusLabel.font = UIFont(name: "Agenda", size: 18)
         self.saysomething.layer.borderWidth = 2
         self.saysomething.layer.borderColor = UIColor.brownColor().CGColor
         // Do any additional setup after loading the view.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWasShown:"), name: UIKeyboardWillChangeFrameNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWasHidden:"), name:UIKeyboardWillHideNotification, object: nil);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func submitFeedback(sender: AnyObject) {
-        
         let params:Dictionary<String,AnyObject> = ["user": config.get("user_id"), "email": "chintan@wohlig.com", "text": saysomething.text]
         
         request.POST(ServerURL+"feed/save", parameters: params, completionHandler: {(response: HTTPResponse) in
-        
+            
             let json = JSON(data: response.responseObject as! NSData)
             print(json);
         })
         
         do {
-//            let opt = try HTTP.POST(ServerURL+"feed/save", parameters: params)
-//            opt.start { response in
-//                print(response.data);
-//                
-//                let json = JSON(data: response.data)
-//                print(json)
-//                //do things...
-//            }
+            //            let opt = try HTTP.POST(ServerURL+"feed/save", parameters: params)
+            //            opt.start { response in
+            //                print(response.data);
+            //
+            //                let json = JSON(data: response.data)
+            //                print(json)
+            //                //do things...
+            //            }
         } catch let error {
             print("got an error creating the request: \(error)")
         }
-        
     }
     
+    func keyboardWasShown(notification: NSNotification) {
+        self.keyboardOn = true;
+        var info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).CGRectValue()
+        
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomConstraint.constant = keyboardFrame.size.height
+        })
+    }
+    
+    func keyboardWasHidden(notification: NSNotification) {
+        keyboardOn = false
+        let info = notification.userInfo!
+        UIView.animateWithDuration(0.1, animations: { () -> Void in
+            self.bottomConstraint.constant = 0
+        })
+    }
 
     /*
     // MARK: - Navigation
